4ddff1b13f99baba551b9bea3468ac7c8fd6be19
aoping <1216791584@qq.com>
2017-04-14 13:15:07 +0800

    gulp自动化构建

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var rev = require('gulp-rev'); // 添加版本号
+var revReplace = require('gulp-rev-replace'); // 更新index里的引用
+var useref = require('gulp-useref'); // 合并css js,在html里设置
+var filter = require('gulp-filter'); // 筛选和恢复
+var uglify = require('gulp-uglify'); // 压缩js
+var csso = require('gulp-csso'); // 压缩css
+
+gulp.task('default',function(){
+	var jsFilter=filter('**/*.js',{restore:true});
+	var cssFilter=filter('**/*.css',{restore:true});
+	var indexHtmlFilter=filter(['**/*','!**/index.html'],{restore:true});
+	return gulp.src('index.html')
+		.pipe(useref())
+		.pipe(jsFilter)
+		.pipe(uglify())
+		.pipe(jsFilter.restore)
+		.pipe(cssFilter)
+		.pipe(csso())
+		.pipe(cssFilter.restore)
+		.pipe(indexHtmlFilter)
+		.pipe(rev())
+		.pipe(indexHtmlFilter.restore)
+		.pipe(revReplace())
+		.pipe(gulp.dest('dist'));
+});
